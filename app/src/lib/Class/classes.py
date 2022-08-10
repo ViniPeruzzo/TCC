e2fc504c4056886422ecfe38f1b27cd60708bee7
Vinicius Peruzzo <viniciusdmp2014@gmail.com>
2022-08-10 14:49:25 -0300

    cm12

--- app/src/lib/Class/classes.py
+++ app/src/lib/Class/classes.py
@@ -4,10 +4,9 @@ import os
 class Img:
     _img_list = []
     _path = os.getcwd()
-    _import_folder_name = ""
-    
+    #_import_folder_name = ""
 
     def __init__(self):
-    
-    def read_imports(self):
         pass
+    def read_imports(self):
+        print(self._path)
